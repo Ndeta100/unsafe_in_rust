5bee411cf3e8375eb40726f1edfdc60fef53600a
Ndeta Innocent <you@example.com>
2022-08-24 16:00:59 +0300

    using get_unchecked method to access array index bounds

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,15 @@ fn main() {
     unsafe trait UnsafeOrd {
         fn cmp(&self, other: &Self) -> Ordering;
     }
+    let array: [u8; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
+    let idx = 7;
+    let results = index(idx, &array);
+    println!("We have {:?} at index {}", results, idx);
+}
+fn index(index: usize, array: &[u8]) -> Option<u8> {
+    if index <= array.len() {
+        unsafe { Some(*array.get_unchecked(index)) }
+    } else {
+        None
+    }
 }
